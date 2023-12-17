d3ccb8c613c037a668dcfe09cf185d23caee23ef
Danixhkeyz <dikushime@gmail.com>
2023-12-17 09:02:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * a script that print exactly what is given
- * Return: Always 0 (Success)
+ * main-A script that print exactly what is given
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
-	return(0)
+        puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 	}
 
